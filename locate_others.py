5dfc025c87c8e7e37ff1c2b1e2f59858211c5876
Kellyhsieh0811 <yufeng.hsie@gmail.com>
2026-04-20 09:32:07 +0800

    chore: 移除暫時的修復腳本

--- locate_others.py
+++
@@ -1,7 +0,0 @@
-import ast
-
-with open('src/app.py', 'r') as f:
-    tree = ast.parse(f.read())
-
-funcs = {node.name: node.lineno for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)}
-print({k: funcs[k] for k in ['ai_rewrite', 'ai_translate', 'ai_extract_topics'] if k in funcs})
